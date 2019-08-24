ce2e9170c47a8c8d3492fdd563a11c871c027697
Dmitriy Davydov <dvdvdmt@gmail.com>
2019-08-24 11:45:51 +0500

    Add a script that updates launchd configs
    Add expansion of a user home directory with bash

--- launchd/update_launchd_configs.sh
+++ launchd/update_launchd_configs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 from_folder="$(dirname "$0")/configs/"
-to_folder="$HOME/Library/LaunchAgents"
+to_folder="$HOME/Library/LaunchAgents/"
 cp -a "$from_folder" "$to_folder"
+find "$HOME/Library/LaunchAgents/" -type f -name -print0 "local.*.plist" \
+| xargs -0 -n 1 launchctl unload
+find "$HOME/Library/LaunchAgents/" -type f -name -print0 "local.*.plist" \
+| xargs -0 -n 1 launchctl load
