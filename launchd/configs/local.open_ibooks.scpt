4ac31ede6f584bdde4458fdc771158b5c9a3078f
Dmitriy Davydov <dvdvdmt@gmail.com>
2019-08-27 08:55:47 +0500

    Replace iBooks by calibre

--- launchd/configs/local.open_ibooks.scpt
+++
@@ -1,8 +0,0 @@
-tell application "iBooks" to activate
-delay 1
-tell application "System Events"
-  key code 125 -- arrow down
-  key code 36 -- enter
-  delay 2
-  keystroke "f" using {command down, control down}
-end tell
--- launchd/update_launchd_configs.sh
+++ launchd/update_launchd_configs.sh
@@ -3,7 +3,5 @@
 from_folder="$(dirname "$0")/configs/"
 to_folder="$HOME/Library/LaunchAgents/"
 cp -a "$from_folder" "$to_folder"
-find "$HOME/Library/LaunchAgents/" -type f -name -print0 "local.*.plist" \
-| xargs -0 -n 1 launchctl unload
-find "$HOME/Library/LaunchAgents/" -type f -name -print0 "local.*.plist" \
-| xargs -0 -n 1 launchctl load
+find "$to_folder" -type f -name "local.*.plist" -print0 | xargs -n 1 -0 launchctl unload
+find "$to_folder" -type f -name "local.*.plist" -print0 | xargs -n 1 -0 launchctl load
