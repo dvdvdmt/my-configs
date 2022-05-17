55a4837def7b7a5a85c8686e8904823c484a789b
Dmitriy Davydov <dvdvdmt@gmail.com>
2022-05-17 17:01:09 +0400

    Fix updating of the karabiner config

--- karabiner/update_macroses.sh
+++ karabiner/update_macroses.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-eval karabiner_configs="~/.config/karabiner/assets/complex_modifications/"
+karabiner_configs="$HOME/.config/karabiner/assets/complex_modifications/"
 
-find `dirname "$0"` -name '*.json' \
+find "$(dirname "$0")" -name '*.json' -print0 \
 | xargs -I'{}' cp '{}' "$karabiner_configs"
